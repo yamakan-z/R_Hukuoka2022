66b68d9304cc07a98f4c5bdab0163d68ab17e078
DEV3-11\GA3 <h1202016006@hiratagakuen.onmicrosoft.com>
2022-12-09 16:33:27 +0900

    ランキング実装準備

--- Assets/yamamoto/Score/Score.cs
+++ Assets/yamamoto/Score/Score.cs
@@ -7,7 +7,9 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //「SCORE」というキーで保存されているInt値を読み込み
+        float resultScore = PlayerPrefs.GetFloat("TEST");
+        Debug.Log("保存されている点数：" + resultScore);
     }
 
     // Update is called once per frame
